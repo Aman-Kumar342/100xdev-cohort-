67fe61c3b194a1f3a75c2b498d22dcd29793831f
Aman-Kumar342 <amantop102525@gmail.com>
2025-03-26 00:15:29 +0530

    newcommit

--- index.js
+++ index.js
@@ -11,13 +11,41 @@
 // console.log(sum(2,3));
 // console.log(product(2,3));
 
-const fs=require('fs');
-const path=require('path');
-console.log(__dirname);
-console.log(path.join(__dirname,"index.js"));
+// const fs=require('fs');
+// const path=require('path');
+// console.log(__dirname);
+// console.log(path.join(__dirname,"index.js"));
 
 
 // const filePath=path.join(__dirname,"index.js");
 // fs.readFile(filePath,'utf8',(err,data)=>{
 
 // }
+
+// assignement 1 for making a command line interface
+
+
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('counter')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
+
+program.command('count')
+  .description('Count the number of lines in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const lines = data.split('\n').length;
+        console.log(`There are ${lines} lines in ${file}`);
+      }
+    });
+  });
+
+program.parse();
