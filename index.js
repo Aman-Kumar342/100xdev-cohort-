2558a953ed5225080d34793fd8428ba8fe2636fc
Aman-Kumar342 <amantop102525@gmail.com>
2025-03-30 23:52:56 +0530

    new one

--- index.js
+++ index.js
@@ -25,27 +25,52 @@
 // assignement 1 for making a command line interface
 
 
-const fs = require('fs');
-const { Command } = require('commander');
-const program = new Command();
+// const fs = require('fs');
+// const { Command } = require('commander');
+// const program = new Command();
 
-program
-  .name('counter')
-  .description('CLI to do file based tasks')
-  .version('0.8.0');
+// program
+//   .name('counter')
+//   .description('CLI to do file based tasks')
+//   .version('0.8.0');
 
-program.command('count')
-  .description('Count the number of lines in a file')
-  .argument('<file>', 'file to count')
-  .action((file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        const lines = data.split('\n').length;
-        console.log(`There are ${lines} lines in ${file}`);
+// program.command('count')
+//   .description('Count the number of lines in a file')
+//   .argument('<file>', 'file to count')
+//   .action((file) => {
+//     fs.readFile(file, 'utf8', (err, data) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         const lines = data.split('\n').length;
+//         console.log(`There are ${lines} lines in ${file}`);
+//       }
+//     });
+//   });
+
+// program.parse();
+
+
+
+
+
+const fs = require("fs");
+
+function main(fileName) {
+  fs.readFile(fileName, "utf-8", function (err, data) {
+    if (err) {
+      console.error("Error reading file:", err);
+      return;
+    }
+    
+    let total = 0;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i] === " ") {
+        total++;
       }
-    });
+    }
+    console.log("Total spaces:", total);
   });
+}
 
-program.parse();
+main("a.txt");
