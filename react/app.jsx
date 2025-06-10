e781525f12a05a073f8bb8b8e48341ce34b07204
Aman Kumar <amantop102525@gmail.com>
2025-06-10 23:57:34 +0530

    todos

--- react/app.jsx
+++ react/app.jsx
@@ -1,16 +1,65 @@
-import *./App,css*;
+// import *./App,css*;
+// export default function App() {
+//     function onClickHandler(){
+//         const currentValue=document.getElementById("btn").innerHTML;
+
+//         const currentCounter=currentValue.split(" ")[1];
+//         const newCounter=parseInt(currentCounter)+1;
+//         document.getElementById("btn").innerHTML=`Click me ${newCounter}`;
+//     }
+//     return(
+//         <div>
+//             <button id="btn" onClick={onClickHandler}>counter 0</button>
+//         </div>
+//     );
+
+//     }
+
+// import{useState} from "react";
+// import "./App.css";
+// export default function App() {
+//     const [counter, setCounter] = useState(0);
+
+//     function onClickHandler() {
+//         setCounter(counter + 1);
+//     }
+
+//     return (
+//         <div>
+//             <button id="btn" onClick={onClickHandler}>
+//                 Click me {counter}
+//             </button>
+//         </div>
+//     );
+// }
+// function Button(props){
+//     return (
+//         <button id="btn" onClick={props.onClick}>
+//             Click me {props.counter}
+//         </button>
+//     );
+// }
+
+
+import { useState } from "react";
+import "./App.css";
 export default function App() {
-    function onClickHandler(){
-        const currentValue=document.getElementById("btn").innerHTML;
-
-        const currentCounter=currentValue.split(" ")[1];
-        const newCounter=parseInt(currentCounter)+1;
-        document.getElementById("btn").innerHTML=`Click me ${newCounter}`;
+    const[todos, setTodos] = useState([
+        {
+            title:"go to gym",
+            description:"hit gym regularly",
+            completed: false
+        },
+    ]);
+    function addTodo() {
+        let newTodo=[];
+        for(let i=0; i<10; i++){
+            newTodo.push({
+                title: `todo ${i}`,
+                description: `description ${i}`,
+                completed: false
+            });
+        }
+        setTodos([...todos, ...newTodo]);
     }
-    return(
-        <div>
-            <button id="btn" onClick={onClickHandler}>counter 0</button>
-        </div>
-    );
-
-    }
+}
